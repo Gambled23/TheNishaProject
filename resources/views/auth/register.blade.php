d3df9ef1a130ce272fa29d29076a09f1a7bbefee
Gambled23 <ipog71@gmail.com>
2023-10-22 10:20:46 -0600

    google login en view de login/register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -56,5 +56,14 @@
                 </x-button>
             </div>
         </form>
+
+        <div class="flex items-center justify-center dark:bg-gray-800 m-3">
+            <form action="{{ route('google.redirect') }}">
+                <button type="submit" class="px-4 py-2 border flex gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150">
+                    <img class="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo">
+                    <span>Register with Google</span>
+                </button>
+            </form>
+        </div>
     </x-authentication-card>
 </x-guest-layout>
