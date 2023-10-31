21ade42d0d9c0df1be261089e8c20e317ca02f3b
Gambled23 <ipog71@gmail.com>
2023-10-30 18:54:47 -0600

    light mode only uwu

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
+<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -29,8 +29,6 @@
                     </x-nav-link>
                 </div>
             </div>
-
-
         </div>
     </div>
 </nav>
