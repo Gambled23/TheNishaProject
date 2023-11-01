c9ebb5c8260713d0e03dfd0155e7a3fd0a6a804b
Gambled23 <ipog71@gmail.com>
2023-10-31 20:07:27 -0600

    botones uwu

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -13,8 +13,9 @@
         </div>
         <div class="mt-6 flex justify-center">
             <form action="{{ route('home') }}">
-                <button class="w-8 h-8 bg-indigo-600 hover:bg-indigo-800 hover:text-white text-xs font-bold uppercase rounded-full transform transition-transform duration-500 hover:w-40 hover:h-8">
-                    <span class="opacity-0 hover:opacity-100">Añadir al carrito</span>
+                <button class="w-8 h-8 bg-indigo-600 hover:bg-indigo-800 text-xs font-bold uppercase rounded-full transition-width duration-100 hover:w-40 hover:h-8">
+                    <i class="fa-solid fa-cart-shopping fa-xl" style="color: #FFFFFF;"></i>
+                    <span class="opacity-0 hover:opacity-100 transition-opacity duration-100 text-white">Añadir al carrito</span>
                 </button>
             </form>
         </div>
