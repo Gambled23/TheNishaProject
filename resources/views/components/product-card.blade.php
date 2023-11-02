196ec99f5517f813ef2cebd9916f4d78be3e8f98
Gambled23 <ipog71@gmail.com>
2023-11-01 19:22:10 -0600

    ignore

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -4,8 +4,7 @@
         <img class="h-48 w-full object-cover" src='{{ URL::to("/assets/{$producto->nombre}0.jpg") }}' alt="{{ $producto->nombre }}">
     </div>
     <div class="p-6">
-        <h2 class="text-2xl font-semibold text-gray-900">{{ $producto->nombre }}</h2>
-        
+        <h2 class="text-2xl font-semibold text-gray-900">{{ $producto->nombre }}</h2>  
         <p class="mt-2 text-sm text-gray-500">{{ \Illuminate\Support\Str::limit($producto->descripcion, 40) }}</p>
         <div class="mt-3">
             <span class="text-sm font-semibold text-gray-700">${{ $producto->precio }}</span>
