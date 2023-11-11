30d0e46a62954100b73d952c992d122c9cced959
Gambled23 <ipog71@gmail.com>
2023-11-11 12:54:09 -0600

    tienda responsiva (nos hemos quedado sin grid mi gente)

--- resources/views/tienda.blade.php
+++ resources/views/tienda.blade.php
@@ -1,14 +1,14 @@
 @extends ('layouts.main')
 @section('body')
 
-<div class="grid grid-cols-3">
+<div class="grid grid-cols-1 md:grid-cols-3">
     <h1 class="text-2xl font-medium text-gray-900 pt-6 mx-6" inline>Todos los productos</h1>
     <x-search-bar/>
 </div>
 
-<div class="grid grid-cols-4 gap-y-6 gap-x-36 mx-5">
+<div class="grid grid-cols-1 md:grid-cols-4 gap-y-6 gap-x-36 mx-5">
 @foreach ($productos as $producto)
-    <x-product-card :producto="$producto" class="w-1/3"/>
+    <x-product-card :producto="$producto" class="w-full md:w-1/3"/>
 @endforeach
 </div>
 
