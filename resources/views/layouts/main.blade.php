2b0cafb6894f712f406500597be2b737f8eab0cf
Gambled23 <ipog71@gmail.com>
2023-11-11 12:53:42 -0600

    cambios a main para hacerla responsiva

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,6 +4,31 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <style>
+        /* Small devices (phones, 640px and down) */
+        @media only screen and (max-width: 640px) {
+            .mx-64 {
+                margin-left: 16px;
+                margin-right: 16px;
+            }
+        }
+
+        /* Medium devices (tablets, 768px and up) */
+        @media only screen and (min-width: 768px) {
+            .mx-64 {
+                margin-left: 64px;
+                margin-right: 64px;
+            }
+        }
+
+        /* Large devices (desktops, 992px and up) */
+        @media only screen and (min-width: 992px) {
+            .mx-64 {
+                margin-left: 128px;
+                margin-right: 128px;
+            }
+        }
+    </style>
 </head>
 
 <body class="
