95c9d21fac48408b6cfb0d832d1007d8a2b4bdb0
Gambled <ipog71@gmail.com>
2023-11-19 09:33:11 -0600

    mínimo ya hay carrousel

--- resources/views/components/photo-carrousel.blade.php
+++ resources/views/components/photo-carrousel.blade.php
@@ -1,32 +1,47 @@
-<div class="max-w-2xl mx-auto">
-<div id="custom-controls-gallery" class="relative w-full" data-carousel="slide">
-    <!-- Carousel wrapper -->
-    <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-        @for ($i = 0; $i < $producto->imagenesTotales; $i++)
-            <div class="{{ $i == 0 ? '' : 'hidden' }} duration-700 ease-in-out" data-carousel-item="{{ $i == 0 ? 'active' : '' }}">
-                <img src='{{ URL::to("/images/{$producto->nombre}{$i}.png") }}' class="absolute block max-w-full h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="{{ $producto->nombre }}">
-            </div>
-        @endfor
-    </div>
-    <div class="flex justify-center items-center pt-4">
-        <button type="button" class="flex justify-center items-center mr-4 h-full cursor-pointer group focus:outline-none" data-carousel-prev>
-            <span class="text-gray-400 hover:text-gray-900 dark:hover:text-white group-focus:text-gray-900 dark:group-focus:text-white">
-                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5H1m0 0 4 4M1 5l4-4"/>
-                </svg>
-                <span class="sr-only">Previous</span>
-            </span>
-        </button>
-        <button type="button" class="flex justify-center items-center h-full cursor-pointer group focus:outline-none" data-carousel-next>
-            <span class="text-gray-400 hover:text-gray-900 dark:hover:text-white group-focus:text-gray-900 dark:group-focus:text-white">
-                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                </svg>
-                <span class="sr-only">Next</span>
-            </span>
-        </button>
-    </div>
-</div>
+<style>
+    .hidden {
+        display: none;
+    }
+</style>
 
-    <script src="https://unpkg.com/flowbite@1.4.0/dist/flowbite.js"></script>
+<div data-carousel="slide">
+    <img src='{{ URL::to("/images/{$producto->nombre}.jpg") }}' data-carousel-item="active" alt="Image 1">
+    <img src='{{ URL::to("/images/{$producto->nombre}1.jpg") }}' class="hidden" alt="Image 2">
+    <img src='image3.jpg' class="hidden" alt="Image 3">
 </div>
+<button data-carousel-prev>Previous</button>
+<button data-carousel-next>Next</button>
+
+<script>
+    // Select the carousel and the buttons
+    let carousel = document.querySelector('[data-carousel="slide"]');
+    let nextButton = document.querySelector('[data-carousel-next]');
+    let prevButton = document.querySelector('[data-carousel-prev]');
+
+    // Add event listeners to the buttons
+    nextButton.addEventListener('click', function() {
+        // Get the current active image
+        let activeImage = carousel.querySelector('[data-carousel-item="active"]');
+        // Get the next image
+        let nextImage = activeImage.nextElementSibling || carousel.firstElementChild;
+
+        // Change the active image
+        activeImage.removeAttribute('data-carousel-item');
+        activeImage.classList.add('hidden');
+        nextImage.setAttribute('data-carousel-item', 'active');
+        nextImage.classList.remove('hidden');
+    });
+
+    prevButton.addEventListener('click', function() {
+        // Get the current active image
+        let activeImage = carousel.querySelector('[data-carousel-item="active"]');
+        // Get the previous image
+        let prevImage = activeImage.previousElementSibling || carousel.lastElementChild;
+
+        // Change the active image
+        activeImage.removeAttribute('data-carousel-item');
+        activeImage.classList.add('hidden');
+        prevImage.setAttribute('data-carousel-item', 'active');
+        prevImage.classList.remove('hidden');
+    });
+</script>
