e037f4196478f2fab8e3dea117cb0891c0846bab
Gambled <ipog71@gmail.com>
2023-11-20 17:24:26 -0600

    about us

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,7 +1,32 @@
 @extends ('layouts.main')
 @section('body')
+<head>
+    <title>Sobre nosotros</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+</head>
 
-
-
+<div class="flex flex-col md:flex-row">
+    <div class="w-full md:w-1/2 p-4">
+        <h2 class="text-2xl font-bold mb-2">Sobre Nisha <i class="fas fa-cat"></i></h2>
+        <p class="text-gray-700">Nisha es un proyecto de crochet por una sóla persona, aunque tenemos un par de productos extras!</p>
+        <p class="text-gray-700">Combinando una estética kawaii y un hermoso estilo de tejido crochet, los productos de Nisha son perfectos regalos y decoraciones para todos los gustos.</p>
+        <p class="text-gray-700">Nisha ofrece una gran variedad de articulos, que van desde pequeños llaveros, hasta peluches y ramos tejidos.</p>
+        <br>
+        <p class="text-gray-700">Además, se aceptan pedidos personalizados!</p>
+        <p class="text-gray-700">Lo único que te separa a ti de uno de estos hermosos productos son un par de clicks :3</p>
+    </div>
+    <div class="w-full md:w-1/2 p-4">
+        <h2 class="text-2xl font-bold mb-2">Sobre el equipo web <i class="fa-solid fa-paw"></i></h2>
+        <p class="text-gray-700">Somos un par de estudiantes de informatica que empezamos este como un proyecto escolar, y lo continuamos como un proyecto hecho con y por amor, intentando combinar el estilo suave de los productos de <b>Nisha</b> con la moderna arquitectura de una página web.</p>
+        <br>
+        <p class="text-gray-700">Mientras <a href="https://github.com/cybness" target="_blank" class="text-violet-700">Vanessa</a> se encargaba de diseñar los mandos de la nave para que nuestra amiga <b>Nisha</b> tuviera una buena experiencia manejando esta página, 
+            <a href="https://github.com/Gambled23" target="_blank" class="text-violet-700">César</a> se esforzó en hacer que la navegación por esta página web sea lo más sencilla y atractiva para ustedes, los usuarios.</p>
+        <br>
+        <p class="text-gray-700">¿Qué dicen? ¿Objetivo logrado?</p>
+    </div>
+</div>
+<div class="flex justify-center text-2xl p-4">
+    <p class="text-gray-700">Esperamos que disfrutes de tu visita!</p>
+</div>
 
 @endsection
--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -23,27 +23,36 @@
     <ul class="space-y-4">
         <li>
             <h3 class="text-lg font-semibold">¿Qué métodos de pago admiten?</h3>
-            <p class="text-gray-600">Pensando en la fácilidad de nuestros clientes, la página admite sólo pagos en PayPal</p>
+            <p class="text-gray-600">Pensando en la fácilidad de nuestros clientes, la página admite sólo pagos en PayPal.</p>
         </li>
         <li>
             <h3 class="text-lg font-semibold">¿En dónde entregan?</h3>
-            <p class="text-gray-600">De momento se hacen entregas en CUCEI, CUCEA y cualquier estación de la linea 3 del tren ligero</p>
+            <p class="text-gray-600">De momento se hacen entregas en CUCEI, CUCEA y cualquier estación de la linea 3 del tren ligero.</p>
         </li>
         <li>
             <h3 class="text-lg font-semibold">¿Cuanto tarda mi pedido en estar listo?</h3>
-            <p class="text-gray-600">La realización del producto puede tardar un par de días hasta un par de semanas, dependiendo la complejidad del producto</p>
+            <p class="text-gray-600">La realización del producto puede tardar varias semanas, dependiendo la complejidad del producto y la carga de trabajo.</p>
         </li>
         <li>
             <h3 class="text-lg font-semibold">¿Cómo saber cuando está listo mi pedido?</h3>
-            <p class="text-gray-600"><b>Nisha</b> se comunicará contigo mediante el correo electronico que usaste para registrarte.<br>Además, es posible consultar el estado de tu pedido desde tu <a href="{{ route('account') }}" class="text-violet-800 hover:text-violet-950">cuenta</a></p>
+            <p class="text-gray-600"><b>Nisha</b> se comunicará contigo mediante el correo electronico que usaste para registrarte.<br>Además, es posible consultar el estado de tu pedido desde tu <a href="{{ route('account') }}" class="text-violet-800 hover:text-violet-950">cuenta</a>.</p>
         </li>
         <li>
             <h3 class="text-lg font-semibold">¿Es posible hacer envíos nacionales?</h3>
-            <p class="text-gray-600">Para envíos por paquetería favor de mandar DM en <a href="https://www.instagram.com/nisha_crochet/" target="_blank" class="text-violet-800 hover:text-violet-950">instagram</a></p>
+            <p class="text-gray-600">Para envíos por paquetería favor de mandar DM en <a href="https://www.instagram.com/nisha_crochet/" target="_blank" class="text-violet-800 hover:text-violet-950">instagram</a>.</p>
         </li>
         <li>
             <h3 class="text-lg font-semibold">¿Hacen pedidos personalizados?</h3>
-            <p class="text-gray-600">Sí!, si quieres hablar sobre un producto personalizado puedes comunicarte por DM en <a href="https://www.instagram.com/nisha_crochet/" target="_blank" class="text-violet-800 hover:text-violet-950">instagram</a></p>
+            <p class="text-gray-600">Sí!, si quieres hablar sobre un producto personalizado puedes comunicarte por DM en <a href="https://www.instagram.com/nisha_crochet/" target="_blank" class="text-violet-800 hover:text-violet-950">instagram</a>. Sujeto a cupos.</p>
+        </li>
+        <li>
+            <h3 class="text-lg font-semibold">¿Cuantos cupos tienen actualmente?</h3>
+            <p class="text-gray-600">Para saber los cupos disponibles en el mes, puedes revisar nuestro perfil de <a href="https://www.instagram.com/nisha_crochet/" target="_blank" class="text-violet-800 hover:text-violet-950">instagram</a>.</p>
+        </li>
+        <li>
+            <h3 class="text-lg font-semibold">¿Es necesario pagar por adelantado?</h3>
+            <p class="text-gray-600">En caso de pedir mediante la página web, sí, pero no te preocupes! Nos aseguramos de usar PayPal para que te sientas más seguro sobre tu dinero.<br>Nunca te vamos a pedir datos sobre tu tarjeta.<br>
+                                       En caso de un pedido personalizado, se aparta el cupo con 50% del precio final.</p>
         </li>
         <li>
             <h3 class="text-lg font-semibold">¿Es posible pedir un reembolso?</h3>
