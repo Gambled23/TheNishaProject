786731d3c23112cd7b20b252368c6c213ab35cd4
Gambled <ipog71@gmail.com>
2023-11-22 14:22:43 -0600

    haiiiii ^_^ hi!! hiiiiii <3 haiiiiii hii :3

---
+++ resources/views/components/errorUnU.blade.php
@@ -0,0 +1,26 @@
+<head>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Kosugi+Maru&display=swap">
+<style>
+    .h1text, .h2text, .ptext {
+        font-family: 'Kosugi Maru', sans-serif !important;
+    }
+    .ptext {
+        padding-top: 8px;
+    }
+</style>
+</head>
+
+<div class="flex items-center justify-center">
+    <div class="p-8 rounded" style="font-family: 'Kosugi Maru';">
+        <h1 class="text-4xl font-bold mb-4 h1text">Nyaaaa (=`ω´=)”</h1>
+        <p class="text-gray-600 ptext">Esto no debería salir (T_T)</p>
+        <p class="text-gray-600"><b>Bakaaaa</b> (＃＞＜) ¿Cómo lograste llegar aquí?</p>
+        <p class="text-gray-600">No deberías estar aquí 人(_ _*)</p>
+        <p class="text-gray-600">Estamos trabajando para resolver este error (￣▽￣*)ゞ</p>
+        <br><br><br><br>
+        <div class=" flex">
+            <h2 class="text-xl h2text">Este fue un mensaje de Nisha</h2> 
+            <x-application-mark/>
+        </div>
+    </div>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,35 +5,51 @@
 <h1 class="text-2xl font-medium text-gray-900 my-2" inline>The Nisha project!</h1>
 
 <div class="pb-8">
+    @if($productos->count() >= 1)
     <a href="{{ route('producto.show', $productos->first()->id ) }}">
     <img class="w-full object-cover" src='{{ URL::to("/images/{$productos->first()->nombre}_0.jpg") }}' alt="{{ $productos->first()->nombre }}">
     </a>
+    @else
+        <x-errorUnU/>
+    @endif
 </div>
 
 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
     <div>
+        @if($productos->count() >= 2)
         <a href="{{ route('producto.show', $productos->skip(1)->first()->id) }}">
         <img class="h-full w-full object-cover" src='{{ URL::to("/images/{$productos->skip(1)->first()->nombre}_0.jpg") }}' alt="{{ $productos->skip(1)->first()->nombre }}">
         </a>
+        @endif
     </div>
     <div>
+        @if($productos->count() >= 3)
         <a href="{{ route('producto.show', $productos->skip(2)->first()->id) }}">
         <img class="h-full w-full object-cover" src='{{ URL::to("/images/{$productos->skip(2)->first()->nombre}_0.jpg") }}' alt="{{ $productos->skip(2)->first()->nombre }}">
         </a>
+        @endif
     </div>
     <div class="grid grid-cols-2">
+        @if($productos->count() >= 4)
         <a href="{{ route('producto.show', $productos->skip(3)->first()->id) }}">
         <img class="h-48 w-full object-cover" src='{{ URL::to("/images/{$productos->skip(3)->first()->nombre}_0.jpg") }}' alt="{{ $productos->skip(3)->first()->nombre }}">
         </a>
+        @endif
+        @if($productos->count() >= 5)
         <a href="{{ route('producto.show', $productos->skip(4)->first()->id) }}">
         <img class="h-48 w-full object-cover" src='{{ URL::to("/images/{$productos->skip(4)->first()->nombre}_0.jpg") }}' alt="{{ $productos->skip(4)->first()->nombre }}">
         </a>
+        @endif
+        @if($productos->count() >= 6)
         <a href="{{ route('producto.show', $productos->skip(5)->first()->id) }}">
         <img class="h-48 w-full object-cover" src='{{ URL::to("/images/{$productos->skip(5)->first()->nombre}_0.jpg") }}' alt="{{ $productos->skip(5)->first()->nombre }}">
         </a>
+        @endif
+        @if($productos->count() >= 7)
         <a href="{{ route('producto.show', $productos->skip(6)->first()->id) }}">
         <img class="h-48 w-full object-cover" src='{{ URL::to("/images/{$productos->skip(6)->first()->nombre}_0.jpg") }}' alt="{{ $productos->skip(6)->first()->nombre }}">
         </a>
+        @endif
     </div>
 </div>
 </div>
