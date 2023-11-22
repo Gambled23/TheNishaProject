4bcd622f04d7bb8ac065a03ccdfdbac26a3cd29d
Gambled <ipog71@gmail.com>
2023-11-22 12:10:26 -0600

    logo color

--- resources/views/components/admin/side-bar.blade.php
+++ resources/views/components/admin/side-bar.blade.php
@@ -1,6 +1,9 @@
     <div class="fixed left-0 top-0 w-64 h-full bg-gray-900 p-4 z-50 sidebar-menu transition-transform">
         <a href="#" class="flex items-center pb-4 border-b border-b-gray-800">
-            <x-application-mark class="block h-9 w-auto" />
+            <svg width="100px" height="40px" width="40px" viewBox="0 0 2 24" fill="none" xmlns="http://www.w3.org/2000/svg" {{
+            $attributes }}> <path d="M14 9V3L11 4H8L5 3V9L9.5 11L14 9ZM14 9L20 12L22 18L20 21H4L2 19L4 17L2 15L4 13M7
+            21V9.88889M11 15V21L16.0435 16H18M8 7L8.00707 7.00707M11 7L11.0071 7.00707" stroke="#e7e7e7" stroke-width="2"
+            stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/> </svg>
             <span class="text-lg font-bold text-white ml-3">N I S H A</span>
         </a>
         <ul class="mt-4">
