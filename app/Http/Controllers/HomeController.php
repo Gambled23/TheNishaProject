9aa2db47393a4f4700c718ca9980d76f0dd54d9b
Aranza Vanessa Perez Navarro <cybergoth2077@outlook.com>
2023-11-23 16:00:05 -0600

    Esta madre requiere pruebas (eager y tabla pedido)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,33 +22,32 @@ class HomeController extends Controller
     
     public function index()
     {
-        $usertype=Auth::user()->usertype;
 
-        if($usertype)
-        {    
-            $clientesViejos = User::where('created_at', '<=', date('Y-m-d H:i:s', strtotime('-30 days')))
-                                    ->where('created_at', '>=', date('Y-m-d H:i:s', strtotime('-60 days')))
-                                    ->count();
-            $clientesNuevos = User::where('created_at', '>=', date('Y-m-d H:i:s', strtotime('-3 days')))->count();
+            // $clientesViejos = User::where('created_at', '<=', date('Y-m-d H:i:s', strtotime('-30 days')))
+            //                         ->where('created_at', '>=', date('Y-m-d H:i:s', strtotime('-60 days')))
+            //                         ->count();
+            // $clientesNuevos = User::where('created_at', '>=', date('Y-m-d H:i:s', strtotime('-3 days')))->count();
             
-            $datos = [
-                'trabajosPendientes' => Trabajos::count(),
-                'clientes' => User::count(),
-                'clientesNuevos' => $clientesNuevos - $clientesViejos,
-                'ordenesActivas' => Pedidos::where('completado', '=', '0')
-                                            ->where('pagado', '=', '1')
-                                            ->count(),
-                'dineroOrdenesActivas' => Pedidos::where('completado', '=', '0')
-                                            ->where('pagado', '=', '1')
-                                            ->sum('precioTotal'),
-                'pedidos' => Pedidos::where('pagado', '=', '1')->latest()->take(15)->get(),
+            // $datos = [
+            //     'trabajosPendientes' => Trabajos::count(),
+            //     'clientes' => User::count(),
+            //     'clientesNuevos' => $clientesNuevos - $clientesViejos,
+            //     'ordenesActivas' => Pedidos::where('completado', '=', '0')
+            //                                 ->where('pagado', '=', '1')
+            //                                 ->count(),
+            //     'dineroOrdenesActivas' => Pedidos::where('completado', '=', '0')
+            //                                 ->where('pagado', '=', '1')
+            //                                 ->sum('precioTotal'),
+            //     'pedidos' => Pedidos::where('pagado', '=', '1')->latest()->take(15)->get(),
 
 
 
-            ];
-            #dd($datos['pedidos']);
-            #dd($datos);
-            return view('admin.dashboard', compact('datos'));
-        }
+            // ];
+            // #dd($datos['pedidos']);
+            // #dd($datos);
+
+            $pedidosdatos = Pedidos::query()->with('user')->latest()->paginate();
+
+            return view('admin.dashboard', compact('pedidosdatos'));
     }
 }
