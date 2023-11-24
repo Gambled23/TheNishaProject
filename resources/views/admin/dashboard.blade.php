1c6a02ca94114a914faea3397b1bf1ac136071cb
Aranza Vanessa Perez Navarro <cybergoth2077@outlook.com>
2023-11-24 14:12:04 -0600

    TENEMOS ACCESORS

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,7 +1,7 @@
 @extends ('layouts.admin')
 @section('body')
     <!-- start: Main -->
-    <main class="w-full md:w-[calc(100%-256px)] md:ml-64 bg-gray-50 min-h-screen transition-all main">
+    <main class="md:w-[calc(100%-256px)] md:ml-64 bg-gray-50 min-h-screen transition-all main">
         
         <?php
         $pagina = "Inicio"
