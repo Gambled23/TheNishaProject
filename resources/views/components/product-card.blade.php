35adc36781c8ad204a704f1d252957f0c89dde8f
Aranza Vanessa Perez Navarro <cybergoth2077@outlook.com>
2023-11-27 19:10:56 -0600

    je

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -2,11 +2,11 @@
     <div class="container mx-auto flex items-center flex-wrap pt-4 pb-12">
         <div class="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col">
             <a href="{{ route('producto.show', $producto)}}">
-                <img class="hover:grow hover:shadow-lg" src='{{ URL::to("/images/{$producto->nombre}_0.jpg") }}' alt="{{ $producto->nombre }}">
+                <img class="hover:grow hover:shadow-lg h-48 w-full object-cover" src='{{ URL::to("/images/{$producto->nombre}_0.jpg") }}' alt="{{ $producto->nombre }}">
                 <div class="pt-3 flex items-center justify-between">
-                    <p class="font-bold">{{ $producto->nombre }}</p>
+                    <p class="font-semibold text-gray-900">{{ $producto->nombre }}</p>
                 </div>
-                <p class="pt-1 text-gray-900">${{ $producto->precio }}</p>
+                <p class="pt-1 font-semibold text-gray-900">${{ $producto->precio }}</p>
                 <div class="mt-6 flex justify-center">
                     <x-carrito-button :producto="$producto"/>
                 </div>
