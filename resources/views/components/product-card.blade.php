c715cb058eb04d1f6b7ac6aff1d2fc1ad8e84037
Gambled <ipog71@gmail.com>
2023-12-05 14:33:43 -0600

    image size

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -2,7 +2,7 @@
     <div class="container mx-auto flex items-center flex-wrap pt-4 pb-12">
         <div class="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col">
             <a href="{{ route('producto.show', $producto)}}">
-                <img class="hover:grow hover:shadow-lg h-48 w-full object-cover" src='{{ URL::to("/images/{$producto->nombre}_0.jpg") }}' alt="{{ $producto->nombre }}">
+                <img class="hover:grow hover:shadow-lg w-full object-cover" src='{{ URL::to("/images/{$producto->nombre}_0.jpg") }}' alt="{{ $producto->nombre }}">
                 <div class="pt-3 flex items-center justify-between">
                     <p class="font-semibold text-gray-900">{{ $producto->nombre }}</p>
                 </div>
